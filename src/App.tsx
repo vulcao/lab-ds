851be6229c7c6a29723ec98dff34e704087ba2d2
Antônio Vulcão <vulcao@gmail.com>
2022-11-21 22:11:11 -0300

    gogogo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+  
+  return (
+    <>
+    <h1 className="font-bold text-2xl text-cyan-500">Hello World</h1>
+
+    <button className='bg-cyan-500 hover:bg-cyan-300 font-medium px-4 rounded text-white'>click</button>
+
+    </>
+  )
+}
+
